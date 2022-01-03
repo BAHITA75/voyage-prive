6515cc6638528fa96e1ec2d1adf2f587fe038289
H-soumia-H <84774174+H-soumia-H@users.noreply.github.com>
2022-01-03 10:19:59 +0100

    initialisation-projet

---
+++ src/Entity/Voyage.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoyageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VoyageRepository::class)
+ */
+class Voyage
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $villeDepart;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $destination;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateDepart;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateRetour;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $dureeSejour;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $prix;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getVilleDepart(): ?string
+    {
+        return $this->villeDepart;
+    }
+
+    public function setVilleDepart(string $villeDepart): self
+    {
+        $this->villeDepart = $villeDepart;
+
+        return $this;
+    }
+
+    public function getDestination(): ?string
+    {
+        return $this->destination;
+    }
+
+    public function setDestination(string $destination): self
+    {
+        $this->destination = $destination;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDateDepart(): ?\DateTimeInterface
+    {
+        return $this->dateDepart;
+    }
+
+    public function setDateDepart(\DateTimeInterface $dateDepart): self
+    {
+        $this->dateDepart = $dateDepart;
+
+        return $this;
+    }
+
+    public function getDateRetour(): ?\DateTimeInterface
+    {
+        return $this->dateRetour;
+    }
+
+    public function setDateRetour(\DateTimeInterface $dateRetour): self
+    {
+        $this->dateRetour = $dateRetour;
+
+        return $this;
+    }
+
+    public function getDureeSejour(): ?int
+    {
+        return $this->dureeSejour;
+    }
+
+    public function setDureeSejour(int $dureeSejour): self
+    {
+        $this->dureeSejour = $dureeSejour;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+}
