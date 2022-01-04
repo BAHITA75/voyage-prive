57abdd915c375a30ef825452d3ffc8530ca30549
abderrahim <a.takabait@hotmail.fr>
2022-01-04 12:28:46 +0100

    dashbord

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -28,11 +28,40 @@ class DashboardController extends AbstractController
         $voyages = $this->entityManager->getRepository(Voyage::class)->findAll();
         $user = $this->entityManager->getRepository(User::class)->findAll();
         
-// dd($voyages);
+
 
         return $this->render('dashboard/dashboard.html.twig', [
            'voyages' => $voyages,
            'user' => $user
+        ]);
+    }
+
+    /**
+     * @IsGranted("ROLE_ADMIN")
+     * @Route("/dashboard/gestion-membre", name="membre")
+     */
+    public function gestionMembre(): Response
+    {
+        
+        $user = $this->entityManager->getRepository(User::class)->findAll();
+        
+
+        return $this->render('dashboard/gestionMembre.html.twig', [
+           
+           'user' => $user
+        ]);
+    }
+
+    /**
+     * @IsGranted("ROLE_ADMIN")
+     * @Route("/dashboard/gestion-voyage", name="voyage")
+     */
+    public function gestionVoyage(): Response
+    {
+        $voyages = $this->entityManager->getRepository(Voyage::class)->findAll();
+        
+        return $this->render('dashboard/gestionVoyage.html.twig', [
+           'voyages' => $voyages,
         ]);
     }
 }
