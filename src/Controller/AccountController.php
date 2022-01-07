fee9b7fe598147fd5dbd907b1818505b06e2b671
H-soumia-H <84774174+H-soumia-H@users.noreply.github.com>
2022-01-07 10:27:16 +0100

    css

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Commande;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccountController extends AbstractController
 {
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/account", name="account")
      */
     public function account(): Response
     {
-        return $this->render('account/account.html.twig');
-    }
+        $commandes = $this->entityManager->getRepository(Commande::class)->findAll();
+        
+        return $this->render('account/account.html.twig', [
+            'commandes' => $commandes,
+         ]);
+
+     
+}
+
 }
